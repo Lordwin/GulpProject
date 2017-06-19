aaf1d9ecd42811405d94e519c51f900e6e66776f
Lordwin <lordwinx@gmail.com>
2017-06-19 15:25:49 +0300

    Rewrite path files to less-->css

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@ var gulp = require('gulp');
 var	concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 var uglify = require('gulp-uglify');
+var less = require('gulp-less');
 //var watch = require('gulp-watch');
 
 gulp.task('watch', function() {
 	gulp.watch('.GulpProject/**/*.css', ['cssConcat']);
 	gulp.watch('.GulpProject/**/*.js', ['jsUglify']);
+
+});
+
+
+gulp.task('less', function() {
+	 gulp.src('./*.less')
+  	.pipe(less())
+    .pipe(gulp.dest('./out/css'));
 
 });
 
