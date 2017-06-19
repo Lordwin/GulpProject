f65d1b367ff0010f00d99fe444ac3383b46ae405
Lordwin <lordwinx@gmail.com>
2017-06-19 12:06:56 +0300

    Install gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp'),
     minifyCSS = require('gulp-css-condense'),
-    notify = require("gulp-notify");
+    notify = require("gulp-notify"),
+    autoprefixer = require('gulp-autoprefixer');
   
  
 gulp.task('default', function() {
     return gulp.src('./style.css')
         .pipe(gulp.dest('./out'))
         .pipe(minifyCSS('minify.css'))   
-        .pipe(notify('Attention! Write changes'));
+        .pipe(notify('Attention! Write changes'))
+        .pipe(autoprefixer('last 15 version'));
 });
 
 gulp.task('watch', function() {
