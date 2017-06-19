04e6dbe30f33c01202032185e0e884043ec4defc
Lordwin <lordwinx@gmail.com>
2017-06-19 12:19:45 +0300

    Added gulp-less and gulp-concat plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     minifyCSS = require('gulp-css-condense'),
     notify = require("gulp-notify"),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    cssNdb = require( 'gulp-css-nbd'),
+    concat = require( 'gulp-concat' );
   
  
 gulp.task('default', function() {
@@ -9,7 +11,10 @@ gulp.task('default', function() {
         .pipe(gulp.dest('./out'))
         .pipe(minifyCSS('minify.css'))   
         .pipe(notify('Attention! Write changes'))
-        .pipe(autoprefixer('last 15 version'));
+        .pipe(autoprefixer('last 15 version'))
+        .pipe( cssNdb() )
+        .pipe( concat( './out/common.css' ) )
+      
 });
 
 gulp.task('watch', function() {
