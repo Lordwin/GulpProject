06bd0db7fa3df23948732d07d3a3d9d8cdaa0f41
Lordwin <lordwinx@gmail.com>
2017-06-19 15:16:55 +0300

    Install gulp-watcher

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require('gulp');
 var	concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 var uglify = require('gulp-uglify');
+//var watch = require('gulp-watch');
+
+gulp.task('watch', function() {
+	gulp.watch('.GulpProject/**/*.css', ['cssConcat']);
+	gulp.watch('.GulpProject/**/*.js', ['jsUglify']);
+
+});
+
 
 gulp.task('jsUglify', function() {
   gulp.src('./scripts/*.js')
